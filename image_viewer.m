329993d0a28ff455eb1928164b3f8851012a65e4
William Jackson <william.jackson@strath.ac.uk>
2019-04-15 14:43:35 +0100

    User entered image pattern

--- image_viewer.m
+++ image_viewer.m
@@ -29,66 +29,73 @@ fig_han.j_pos_str = uicontrol('Style','edit','String','0',...
 fig_han.k_pos_str = uicontrol('Style','edit','String','0',...
     'Position',[1330,400,30,25]);
 
-fig_han.base_test = uicontrol('Style','text','String','Base File Name',...
-    'Position',[1300,300,150,25],'HorizontalAlignment','left');
+fig_han.base_test = uicontrol('Style','text','String',...
+    'File Name Pattern', 'Position',[1300,300,150,25],...
+    'HorizontalAlignment','left');
 
-fig_han.base_name = uicontrol('Style','edit','String','image',...
-    'Position',[1300,280,100,25]);
-
-fig_han.base_test = uicontrol('Style','text','String','File Extension',...
-    'Position',[1300,240,100,25],'HorizontalAlignment','left');
-
-fig_han.file_type = uicontrol('Style','edit','String','.bmp',...
-    'Position',[1300,220,80,25]);
+fig_han.base_name = uicontrol('Style','edit','String',...
+    'image_{}_{}_{}.bmp',...
+    'Position',[1300,280,150,25]);
 
 fig_han.load = uicontrol('Style','pushbutton','String','Load',...
-    'Position',[1300,160,60,25],...
+    'Position',[1300,240,60,25],...
     'Callback', {@load_reset_Callback});
 
 fig_han.image_path = '';
 
+fig_han.enabled = 0;
+fig_han.message_shown = 0;
+
 
     function key_press(~, eventdata)
         
-        draw_it = 0;
-        ii = str2double(fig_han.i_pos_str.String);
-        jj = str2double(fig_han.j_pos_str.String);
-        kk = str2double(fig_han.k_pos_str.String);
-        
-        curr_inc_i = str2double(fig_han.i_inc_str.String);
-        curr_inc_j = str2double(fig_han.j_inc_str.String);
-        curr_inc_k = str2double(fig_han.k_inc_str.String);
-        
-        
-        switch eventdata.Key
+        if fig_han.enabled
             
-            case('leftarrow')
-                ii = ii - curr_inc_i;
-                draw_it = 1;
-            case('rightarrow')
-                ii = ii + curr_inc_i;
-                draw_it = 1;
-            case('uparrow')
-                jj = jj + curr_inc_j;
-                draw_it = 1;
-            case('downarrow')
-                jj = jj - curr_inc_j;
-                draw_it = 1;
-            case('pageup')
-                kk = kk + curr_inc_k;
-                draw_it = 1;
-            case('pagedown')
-                kk = kk - curr_inc_k;
-                draw_it = 1;
+            draw_it = 0;
+            ii = str2double(fig_han.i_pos_str.String);
+            jj = str2double(fig_han.j_pos_str.String);
+            kk = str2double(fig_han.k_pos_str.String);
+            
+            curr_inc_i = str2double(fig_han.i_inc_str.String);
+            curr_inc_j = str2double(fig_han.j_inc_str.String);
+            curr_inc_k = str2double(fig_han.k_inc_str.String);
+            
+            
+            switch eventdata.Key
+                
+                case('leftarrow')
+                    ii = ii - curr_inc_i;
+                    draw_it = 1;
+                case('rightarrow')
+                    ii = ii + curr_inc_i;
+                    draw_it = 1;
+                case('uparrow')
+                    jj = jj + curr_inc_j;
+                    draw_it = 1;
+                case('downarrow')
+                    jj = jj - curr_inc_j;
+                    draw_it = 1;
+                case('pageup')
+                    kk = kk + curr_inc_k;
+                    draw_it = 1;
+                case('pagedown')
+                    kk = kk - curr_inc_k;
+                    draw_it = 1;
+            end
+            
+            if draw_it
+                fig_han.i_pos_str.String = num2str(ii);
+                fig_han.j_pos_str.String = num2str(jj);
+                fig_han.k_pos_str.String = num2str(kk);
+                load_and_display()
+            end
+            
+        else
+            if not(fig_han.message_shown)
+                disp('Click load to enable scrolling with keys')
+                fig_han.message_shown = 1;
+            end
         end
-        
-        if draw_it
-            fig_han.i_pos_str.String = num2str(ii);
-            fig_han.j_pos_str.String = num2str(jj);
-            fig_han.k_pos_str.String = num2str(kk);
-            load_and_display()
-        end
-        
     end
 
     function load_reset_Callback(~, ~)
@@ -98,21 +105,41 @@ fig_han.image_path = '';
 
     function get_file_name()
         
-        fig_han.image_path = sprintf('%s_%s_%s_%s%s',....
-            fig_han.base_name.String, fig_han.i_pos_str.String,...
-            fig_han.j_pos_str.String, fig_han.k_pos_str.String,...
-            fig_han.file_type.String);
+        input_string = fig_han.base_name.String;
+        splits = strsplit(input_string,'{}');
+        len = length(splits);
+        if len>1 && len <=4
+            im_string = "";
+        else
+            disp('Invalid image string')
+            return
+        end
+        
+        number_strings = {fig_han.i_pos_str.String,...
+            fig_han.j_pos_str.String, fig_han.k_pos_str.String};
+        
+        for ii = 1:len-1
+            im_string = im_string + splits{ii};
+            im_string = im_string + number_strings{ii};
+        end
+        
+        im_string = im_string + splits{end};
+        
+        fig_han.image_path = char(im_string);
+        
     end
 
 
     function load_and_display()
+        
+        fig_han.enabled = 1;
         
         get_file_name();
         
         try
             image = imread(fig_han.image_path);
             imshow(image);
-        catch %catch them all yolo
+        catch % should rethrow errors that arent file not found...
             disp('Image not found')
             disp(fig_han.image_path);
             return
